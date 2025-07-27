f556ca562335f327b218a1ac1dee1b1a3e61266a
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-07-27 23:34:12 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.font_editor_pro"
-    compileSdk = 34
+    compileSdk = 35
     
     defaultConfig {
         applicationId = "com.example.font_editor_pro"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
