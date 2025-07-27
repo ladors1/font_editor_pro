808827b2b7dd1d215b840ca8e4e0f008ee0ed315
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-07-27 21:57:47 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    // نسخه پلاگین اندروید را به 7.3.0 که سازگاری بیشتری با پکیج‌های قدیمی دارد، تغییر می‌دهیم
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    // از نسخه‌های پیش‌فرض و مدرن استفاده می‌کنیم
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
