e9c630f3cf8bd5a72ec11a6e3cb0bfe9c2f04153
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-07-27 22:40:04 +0330

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/font_editor_pro/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/font_editor_pro/MainActivity.kt
@@ -2,4 +2,7 @@ package com.example.font_editor_pro
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // این کلاس به صورت عمدی خالی است.
+    // تمام قابلیت‌های مدرن در کلاس والد یعنی FlutterActivity پیاده‌سازی شده است.
+}
